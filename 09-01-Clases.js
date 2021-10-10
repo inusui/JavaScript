2d47aa079335a1fe55a1be4efd795894e48d665f
inusui <radg21@outlook.es>
2021-10-10 15:39:02 -0500

    09-01-Clases 10/Oct 15:38

--- 09-01-Clases.js
+++ 09-01-Clases.js
@@ -1,11 +1,23 @@
 class Persona {
     constructor(nombre , apellido){//pasar parametros (aqui)
-        this.nombre = nombre; //Hace referencia a una nueva propiedad (lado Izquierdo) Se le asigna el parametro que entra a la clase (lo que esta arriba)
-        this.apellido = apellido;
+        this._nombre = nombre; 
+        //Hace referencia a una nueva propiedad (lado Izquierdo) Se le asigna el parametro que entra a la clase (lo que esta arriba)
+        this._apellido = apellido;
+    }
+
+    get nombre (){ //El nombre no debe ser el mismo que nuestros argumentos. 
+        return this._nombre;
+    }
+
+    set nombre (nombre){
+        this._nombre = nombre;
     }
 }//Clase de Tipo Persona
 
-let persona1 = new Persona('Juan', 'Perez'); //Reservar espacio para crear objeto Persona, se le llama instancia de clase o objeto, llama al constructor de la clase. 
-
-console.log (persona1);
-
+let persona1 = new Persona('Juan', 'Perez'); 
+console.log(persona1._nombre)
+//Reservar espacio para crear objeto Persona, se le llama instancia de clase o objeto, llama al constructor de la clase. 
+persona1.nombre = 'Juancito'
+console.log (persona1.nombre);
+let persona2 = new Persona("Pelin", "Cusi");
+console.log(persona2);
